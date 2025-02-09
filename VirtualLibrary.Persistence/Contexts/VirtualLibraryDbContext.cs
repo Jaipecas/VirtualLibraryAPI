a491c82683c9142b4dc13be3755e731fd5e6c984
Jaime Pérez Castellanos <jaipecas89@gmail.com>
2025-02-09 11:39:16 +0100

    fte: configure entity and identity

---
+++ VirtualLibrary.Persistence/Contexts/VirtualLibraryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VirtualLibrary.Domain;
+
+namespace VirtualLibrary.Persistence.Contexts
+{
+    public class VirtualLibraryDbContext : IdentityDbContext<IdentityUser>
+    {
+        public VirtualLibraryDbContext(DbContextOptions<VirtualLibraryDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
