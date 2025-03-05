3e47b163bb2591cc8875db1f7d5faeee53732f21
Jaime Pérez Castellanos <jaipecas89@gmail.com>
2025-03-05 20:02:40 +0100

    fte: add lazy loader to studyRoom

--- VirtualLibrary.Domain/StudyRoomEntities/StudyRoom.cs
+++ VirtualLibrary.Domain/StudyRoomEntities/StudyRoom.cs
@@ -1,14 +1,36 @@
 ﻿
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace VirtualLibrary.Domain.StudyRoomEntities
 {
     public class StudyRoom : GenericEntity
     {
+        private readonly ILazyLoader _lazyLoader;
+
         public required string Name { get; set; }
         public required string Description { get; set; }
-        public required List<StudyRoomUser> StudyRoomUsers { get; set; }
-        public required Pomodoro Pomodoro { get; set; }
+
+        private List<StudyRoomUser>? _studyRoomUsers;
+        public List<StudyRoomUser> StudyRoomUsers
+        {
+            get => _lazyLoader.Load(this, ref _studyRoomUsers);
+            set => _studyRoomUsers = value;
+        }
+
+        private Pomodoro? _pomodoro;
+        public Pomodoro Pomodoro
+        {
+            get => _lazyLoader.Load(this, ref _pomodoro);
+            set => _pomodoro = value;
+        }
+
         public required string OwnerId { get; set; }
-        public required User Owner { get; set; }
+        private User? _owner;
+        public User Owner
+        {
+            get => _lazyLoader.Load(this, ref _owner);
+            set => _owner = value;
+        }
+        public StudyRoom(ILazyLoader lazyLoader) => _lazyLoader = lazyLoader;
     }
 }
