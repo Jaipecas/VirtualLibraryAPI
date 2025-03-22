320db71c0d2c884e8173c72b1e00b714e78cecd1
Jaime Pérez Castellanos <jaipecas89@gmail.com>
2025-03-22 18:26:55 +0100

    fte: create notification entity

---
+++ VirtualLibrary.Domain/StudyRoomEntities/Notification.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace VirtualLibrary.Domain.StudyRoomEntities
+{
+    public class Notification
+    {
+        public required string SenderId { get; set; }
+
+        private User? _sender;
+        public User Sender
+        {
+            get => _lazyLoader.Load(this, ref _sender);
+            set => _sender = value;
+        }
+
+        public required string RecipientId { get; set; }
+        private User? _recipient;
+        public User Recipient
+        {
+            get => _lazyLoader.Load(this, ref _recipient);
+            set => _recipient = value;
+        }
+
+        public required string Title { get; set; }
+        public required string Message { get; set; }
+
+
+        private readonly ILazyLoader _lazyLoader;
+        public Notification(ILazyLoader lazyLoader) => _lazyLoader = lazyLoader;
+    }
+}
