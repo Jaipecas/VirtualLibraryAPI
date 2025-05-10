fa49e54a64ef412893ef15fd904e9ce45dee73a3
Jaime Pérez Castellanos <jaipecas89@gmail.com>
2025-05-10 20:11:49 +0200

    fte: patron result UpdateRoomTimer

--- VirtualLibraryAPI/Controllers/Features/StudyRoomFeatures/StudyRoomController.cs
+++ VirtualLibraryAPI/Controllers/Features/StudyRoomFeatures/StudyRoomController.cs
@@ -73,7 +73,12 @@ namespace VirtualLibraryAPI.Controllers.Features.StudyRoomFeatures
         [HttpPut("updateRoomTimer")]
         public async Task<IActionResult> UpdateRoomTimer(UpdateRoomTimerCommand command)
         {
-            return await _mediator.Send(command);
+            var result = await _mediator.Send(command);
+
+            if (!result.IsSuccess)
+                return BadRequest(new { result.Errors });
+
+            return Ok(result.Value);
         }
     }
 }
