aad6c79d155726e826aeb910e5afa1d6c1aba227
Jaime Pérez Castellanos <jaipecas89@gmail.com>
2025-05-21 12:25:04 +0200

    pruebas azure

--- VirtualLibraryAPI/Program.cs
+++ VirtualLibraryAPI/Program.cs
@@ -79,7 +79,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")
+            policy.WithOrigins("https://localhost:5173")
                   .AllowCredentials()
                   .AllowAnyMethod()
                   .AllowAnyHeader();
